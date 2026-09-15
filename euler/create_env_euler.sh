c7181c9074747df12372aca8b8d838cf72a38c6a
Christoph Sven Funke <csfunke@yahoo.com>
2026-09-15 17:55:18 +0200

    Add euler support

---
+++ euler/create_env_euler.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# activate the right modules
+# gcc 12.2 stack
+module load stack/2024-06
+module load gcc/12.2.0
+# python 3.11.2
+module load python/3.11.6
+# backend for the solvers
+module load glpk
+module load gurobi/12.0.1
+
+# create the env
+python -m venv zen_garden_plugins_env
+
+# activate the env
+source zen_garden_plugins_env/bin/activate
+
+# install the requirements
+pip install -U pip
+pip install uv
+pip install -e ..[dev,docs]
+pip install gurobipy==12.0.1
+
+# source zen_garden_plugins_env/bin/activate
+# cd path/to/zen-garden
+# uv pip install -e .[dev,vis,docs]
---
+++ euler/source_env_euler.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# actiavte the right modules
+# gcc 12.2 stack
+module load stack/2024-06
+module load gcc/12.2.0
+
+# python 3.11.6
+module load python/3.11.6
+
+# backend for the solvers
+module load glpk
+module load gurobi/12.0.1
+
+# activate the env from anywhere
+# $BASH_SOURCE contains the path that was sourced
+source $(realpath $BASH_SOURCE | xargs dirname)/zen_garden_plugins_env/bin/activate
